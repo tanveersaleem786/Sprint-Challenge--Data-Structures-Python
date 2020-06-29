154ed372f4a8d87800cbea73f35ec75bc88b8e21
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-06-28 21:50:01 -0400

    Stretch Completed by Using Binary Search Tree Data Structure

--- names/names.py
+++ names/names.py
@@ -15,14 +15,38 @@ duplicates = []  # Return the list of duplicates in this data structure
 
 # Its time complexity is O(n^2)
 # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+# Using a binary search tree data structure
+# Build a binary tree
+# Total time complexity of this solution is O(n log n)
+bst = None
+
+# It's time complexity is O(n)
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    
+    if bst:
+        bst.insert(name_1)
+    else:
+        bst = BSTNode(name_1)
 
-
+# Search duplication in binary tree
+# It's time complexity is log(n)            
+for name_2 in names_2:
+    
+    if bst.contains(name_2):
+        duplicates.append(name_2)
 
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+# ---------- Stretch Goal -----------
+# Python has built-in tools that allow for a very efficient approach to this problem
+# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# structures, but you may not import any additional libraries that you did not write yourself.
